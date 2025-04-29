c721aad303c3125de43ebb78dc3055a2e5aff092
Musabbihakamal <167633994+Musabbihakamal@users.noreply.github.com>
2025-04-30 04:59:45 +0500

    Create 02_print_event.py

---
+++ loop_control_flow/02_print_event.py
@@ -0,0 +1,9 @@
+# Initialize a variable to keep track of the count
+count = 0
+
+# Loop to print the first 20 even numbers
+while count < 20:
+    even_number = count * 2  # Calculate the even number by multiplying the count by 2
+    print(even_number, end=" ")  # Print the even number with space
+    count += 1  # Increment the count
+
