70cd04f265cdc8f4595c90e155379a3df5ea0003
Musabbihakamal <167633994+Musabbihakamal@users.noreply.github.com>
2025-04-30 04:42:35 +0500

    Create 01_print_event.py

---
+++ if_statement/01_print_event.py
@@ -0,0 +1,8 @@
+def print_even_numbers():
+    # Loop through numbers from 0 to 19
+    for i in range(20):
+        # Multiply each number by 2 to get the even number and print it on the same line
+        print(i * 2, end=" ")
+
+# Call the function to print the first 20 even numbers
+print_even_numbers()
