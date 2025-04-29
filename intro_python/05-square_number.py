90dc58b56f24668e92208c7bd589186420b183b5
Musabbihakamal <167633994+Musabbihakamal@users.noreply.github.com>
2025-04-30 04:14:35 +0500

    Create 05-square_number.py

---
+++ intro_python/05-square_number.py
@@ -0,0 +1,12 @@
+def main():
+    # Ask the user to type a number
+    number = float(input("Type a number to see its square: "))
+    
+    # Calculate the square of the number
+    square = number ** 2
+    
+    # Print the result
+    print(f"{number} squared is {square}")
+
+# Calling the main function to run the program
+main()
