6832cb3a62c411db323eaf0e909e39c62b51fe4f
Musabbihakamal <167633994+Musabbihakamal@users.noreply.github.com>
2025-04-30 05:10:03 +0500

    Create 04_get_name.py

---
+++ functions/04_get_name.py
@@ -0,0 +1,11 @@
+def get_name():
+    # Return the name as a string
+    return "Sophia"
+
+def main():
+    name = get_name()
+    print(f"Howdy {name} ! 🤠")
+
+# Required to run the main function
+if __name__ == '__main__':
+    main()
