b0076366c31b8ad8f993fd77cb2eb1fd7c103358
Musabbihakamal <167633994+Musabbihakamal@users.noreply.github.com>
2025-04-30 05:15:26 +0500

    Create 09_print_ones_digit.py

---
+++ functions/09_print_ones_digit.py
@@ -0,0 +1,15 @@
+def print_ones_digit(num):
+    # Get the ones digit using modulo and print it
+    ones_digit = num % 10
+    print(f"The ones digit is {ones_digit}")
+
+def main():
+    try:
+        num = int(input("Enter a number: "))
+        print_ones_digit(num)
+    except ValueError:
+        print("Please enter a valid integer.")
+
+if __name__ == '__main__':
+    main()
+
